9d1d7fe21dbcad2821531f2f8fabacb55002c03a
vaperoblox <96341271+vaperoblox@users.noreply.github.com>
2022-05-28 18:08:32 +0100

    Add files via upload

---
+++ NewMainScript.lua
@@ -0,0 +1,2 @@
+if shared.LoadPrivate then shared.VapePrivate, shared.SECRETKEY = true, "VAPEPRIVATE_PJK78TSG1HQUGRKVIMPA" end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
