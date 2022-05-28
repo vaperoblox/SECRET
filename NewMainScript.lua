5e1708de195043c43ebe49b6654d2b555b10406e
vaperoblox <96341271+vaperoblox@users.noreply.github.com>
2022-05-28 18:25:25 +0100

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,2 +1,3 @@
 if shared.LoadPrivate then shared.VapePrivate, shared.SECRETKEY = true, "VAPEPRIVATE_PJK78TSG1HQUGRKVIMPA" end
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vaperoblox/SECRET/main/bedwars.lua", true))() -- Fixed private haha, engo is trash.
